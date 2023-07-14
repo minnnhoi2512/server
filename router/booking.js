98537c358c30ded79039391cf972d8ce69c02c04
minnnhoi2512 <133669352+minnnhoi2512@users.noreply.github.com>
2023-07-14 10:28:12 +0700

    update_v5

--- router/booking.js
+++ router/booking.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-
+import Auth from "../middleware/auth.js";
 import * as bookingController from '../controllers/booking.js'
 const router = Router();
 
-router.route('/createBooking').post(bookingController.createBooking);
+router.route('/createBooking').post(Auth,bookingController.createBooking);
 router.route('/getAllBookings').get(bookingController.getAllBookings);
 router.route('/deleteBooking/:id').delete(bookingController.deleteBooking);
 router.route('/updateBooking/:id').post(bookingController.updateBooking);
