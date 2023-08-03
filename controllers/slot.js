cca3f961d8d7743eb98f4064cdf393713fc4e8b1
minnnhoi2512 <133669352+minnnhoi2512@users.noreply.github.com>
2023-08-04 00:03:58 +0700

    SWP

--- controllers/slot.js
+++ controllers/slot.js
@@ -1,6 +1,6 @@
 //CRUD slot 
 import SlotModel from "../model/Slot.model.js";
-
+import {DateTime} from 'luxon'
 export async function createSlot(req, res) {
     const {
         grade,
@@ -11,23 +11,26 @@ export async function createSlot(req, res) {
     } = req.query
     try {
         debugger
-        let day = new Date(date);
-        day.setHours(7);
-        day.setMinutes(0);
-        day.setSeconds(0);
-        day.setMilliseconds(0);
+        // let day = new Date("yyyy/mm/dd").toLocaleString("en-CA",{timeZone:"Asia/Ho_Chi_Minh"});
+        const today = DateTime.now().setZone('Asia/Ho_Chi_Minh').toISODate();
+        // day.getDate();
+        // day.setHours(7);
+        // day.setMinutes(0);
+        // day.setSeconds(0);
+        // day.setMilliseconds(0);
         // debugger
-        const existSlot = await SlotModel.findOne({ grade: grade, user: user, date: day })
+        // let element = day.toString().split(",");
+        const existSlot = await SlotModel.findOne({ grade: grade, user: user, date: today })
         if (existSlot != null) {
             existSlot.isAttended = isAttended || existSlot.isAttended;
             await existSlot.save();
             res.status(200).json(existSlot)
         } else {
-
+            // 2023-08-03T09:29:52.365Z
             const newSlot = await SlotModel.create({
                 grade,
                 user,
-                date: day,
+                date: today,
                 isAttended: isAttended || 0,
             })
             // debugger
@@ -36,6 +39,7 @@ export async function createSlot(req, res) {
 
 
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             msg: 'Failed haha   '
         })
@@ -47,6 +51,19 @@ export async function getAllSlotOfUser(req, res) {
     try {
         // debugger
         const allSlot = await SlotModel.find({ user: id })
+        res.status(200).json(allSlot)
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Sever error !!!'
+        })
+    }
+}
+export async function getAllSlotInDay(req, res) {
+    const grade = req.query.grade
+    const date = req.query.date
+    try {
+        // debugger
+        const allSlot = await SlotModel.find({ grade: grade,date : date }).populate("user").populate("grade")
         res.status(200).json(allSlot)
     } catch (error) {
         res.status(500).json({
--- router/slot.js
+++ router/slot.js
@@ -3,4 +3,5 @@ import * as SlotController from '../controllers/slot.js'
 const router = Router();
 router.route('/createSlot').post(SlotController.createSlot); // cai nay moi' ne`
 router.route('/getSlot/:id').get(SlotController.getAllSlotOfUser); // cai nay moi' ne`
+router.route('/getAllSlotInDay').get(SlotController.getAllSlotInDay); // cai nay moi' ne`
 export default router
